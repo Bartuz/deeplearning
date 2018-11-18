c170059faf086e3e83f5e1bd741727ac77765605
Filip Bartuzi <filip.bartuzi@gmail.com>
2018-11-18 16:50:06 +0100

    Add data encoding (categorical data)

--- ml-basic/data-process-template/data_preprocessing_template.py
+++ ml-basic/data-process-template/data_preprocessing_template.py
@@ -10,7 +10,13 @@ dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
-from sklearn.preprocessing import SimpleImputer
-imputer = SimpleImputer(missing_values='NaN', strategy='mean', axis=0)
+# Fill missing data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values='NaN', strategy='mean', axis=0)
 imputer = imputer.fit(X[:, 1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
+
+# Change text (like Yes / No) to categorical data (1/0)
+from sklearn.preprocessing import LabelEncoder
+labelEncoder_X = LabelEncoder()
+X[:, 0] = labelEncoder_X.fit_transform(X[:, 0])

266f3833eefe213fa858ebeb809dd29b5e5359a9
Filip Bartuzi <filip.bartuzi@gmail.com>
2018-11-18 20:36:09 +0100

    Add standardized train and test daa of churn_bank.csv

--- ann/churn.py
+++ ann/churn.py
@@ -18,4 +18,15 @@ labelencoder_X_2 = LabelEncoder()
 X[:, 2] = labelencoder_X_1.fit_transform(X[:, 2])
 onehotencoder = OneHotEncoder(categorical_features = [1])
 X = onehotencoder.fit_transform(X).toarray()
+# Fix dummy variable trap
 X = X[:, 1:]
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)

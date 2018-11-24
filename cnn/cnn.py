a38fc5b85d80ec9c70245bec8e1f83d572334437
Filip Bartuzi <filip.bartuzi@gmail.com>
2018-11-24 21:01:13 +0100

    Initialize CNN :)

---
+++ cnn/cnn.py
@@ -0,0 +1,10 @@
+# Part 1 - Building CNN
+
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPool2D
+from keras.layers import Flatten
+from keras.layers import Dense
+
+# Initialize
+classifier = Sequential()
